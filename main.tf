3df943f03a4663efd9519dccdd718adef8d5c8e6
kaumudi <kaumudi766@gmail.com>
2022-11-16 10:19:04 +0530

    Initial commit of modules repo

--- main.tf
+++ main.tf
@@ -1,137 +1,14 @@
-resource "aws_launch_configuration" "example" {
-  image_id        = "ami-0fb653ca2d3203ac1"
-  instance_type   = var.instance_type
-  security_groups = [aws_security_group.instance.id]
-
-  user_data = templatefile("user-data.sh", {
-    server_port = var.server_port
-    db_address  = data.terraform_remote_state.db.outputs.address
-    db_port     = data.terraform_remote_state.db.outputs.port
-  })
-  # Required when using a launch configuration with an ASG.
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-resource "aws_autoscaling_group" "example" {
-  launch_configuration = aws_launch_configuration.example.name
-  vpc_zone_identifier  = data.aws_subnets.default.ids
-  target_group_arns    = [aws_lb_target_group.asg.arn]
-  health_check_type    = "ELB"
-
-  min_size = var.min_size
-  max_size = var.max_size
-
-  tag {
-    key                 = "Name"
-    value               = var.cluster_name
-    propagate_at_launch = true
-  }
-}
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.example.arn
-  port              = 80
-  protocol          = "HTTP"
-
-  # By default, return a simple 404 page
-  default_action {
-    type = "fixed-response"
-
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "404: page not found"
-      status_code  = 404
-    }
-  }
-}
-resource "aws_security_group" "alb" {
-  name = "${var.cluster_name}-alb"
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-locals {
-  http_port    = 80
-  any_port     = 0
-  any_protocol = "-1"
-  tcp_protocol = "tcp"
-  all_ips      = ["0.0.0.0/0"]
-}
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.example.arn
-  port              = local.http_port
-  protocol          = "HTTP"
-
-  # By default, return a simple 404 page
-  default_action {
-    type = "fixed-response"
-
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "404: page not found"
-      status_code  = 404
-    }
-  }
-}
-resource "aws_security_group" "alb" {
-  name = "${var.cluster_name}-alb"
-  ingress {
-    from_port   = local.http_port
-    to_port     = local.http_port
-    protocol    = local.tcp_protocol
-    cidr_blocks = local.all_ips
-  }
-  user_data = templatefile("${path.module}/user-data.sh", {
-  server_port = var.server_port
-  db_address  = data.terraform_remote_state.db.outputs.address
-  db_port     = data.terraform_remote_state.db.outputs.port
-})
 
 
-  egress {
-    from_port   = local.any_port
-    to_port     = local.any_port
-    protocol    = local.any_protocol
-    cidr_blocks = local.all_ips
-  }
-}
-resource "aws_security_group" "alb" {
-  name = "${var.cluster_name}-alb"
+module "webserver_cluster" {
+  source = "github.com/foo/modules//services/webserver-cluster?ref=v0.0.2"
 
-  ingress {
-    from_port   = local.http_port
-    to_port     = local.http_port
-    protocol    = local.tcp_protocol
-    cidr_blocks = local.all_ips
-  }
+  cluster_name           = "webservers-stage"
+  db_remote_state_bucket = "kaumudi-tf-test-bucketstate1"
+  db_remote_state_key    = "stage/data-stores/mysql/terraform.tfstate"
 
-  egress {
-    from_port   = local.any_port
-    to_port     = local.any_port
-    protocol    = local.any_protocol
-    cidr_blocks = local.all_ips
-  }
-}
-resource "aws_security_group" "alb" {
-  name = "${var.cluster_name}-alb"
+  instance_type = "t2.micro"
+  min_size      = 2
+  max_size      = 2
 }
 
-resource "aws_security_group_rule" "allow_all_outbound" {
-  type              = "egress"
-  security_group_id = aws_security_group.alb.id
-
-  from_port   = local.any_port
-  to_port     = local.any_port
-  protocol    = local.any_protocol
-  cidr_blocks = local.all_ips
-}
